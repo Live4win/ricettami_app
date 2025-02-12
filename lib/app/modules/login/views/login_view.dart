fbdb5838ba7c15d8049013d5197d7951e83639a9
Emmanuel Baah <baahemm@dialog-europe.com>
2025-02-12 16:16:59 +0100

    Implemented login screen

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,120 +9,145 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 46,
-            children: [
-              Image.asset(
-                'assets/full_ricettami_logo.png',
-                width: 256,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DecoratedBox(
+              position: DecorationPosition.foreground,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.1),
+                ),
+                borderRadius: BorderRadius.circular(8),
+                /*boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 8,
+                          offset: Offset(0, 4),
+                        ),
+                      ],*/
               ),
-              //Spacer(),
-              SizedBox(
-                width: 256,
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  //mainAxisSize: MainAxisSize.min,
-                  spacing: 24,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 46,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 8,
-                      children: [
-                        Text('Email'),
-                        TextField(
-                          controller: controller.emailController,
-                          decoration: InputDecoration(
-                            hintText: 'Inserisci la mail',
-                            hintStyle: TextStyle(
-                              color: Colors.grey,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
+                    Image.asset(
+                      'assets/full_ricettami_logo.png',
+                      width: 256,
+                    ),
+                    //Spacer(),
+                    SizedBox(
+                      width: 300,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 24,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            spacing: 8,
+                            children: [
+                              Text('Email'),
+                              TextField(
+                                controller: controller.emailController,
+                                decoration: InputDecoration(
+                                  hintText: 'Inserisci la mail',
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            spacing: 8,
+                            children: [
+                              Text('Password'),
+                              TextField(
+                                controller: controller.passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  hintText: 'Inserisci la password',
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          TextButton(
+                            onPressed: () => controller.forgotPassword(),
+                            child: Text(
+                              'Password dimenticata?',
+                              /*style: TextStyle(
+                                decoration: TextDecoration.underline,
+                              ),*/
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 8,
-                      children: [
-                        Text('Password'),
-                        TextField(
-                          controller: controller.passwordController,
-                          decoration: InputDecoration(
-                            hintText: 'Inserisci la password',
-                            hintStyle: TextStyle(
-                              color: Colors.grey,
-                            ),
-                            /*focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors
-                                    .blue, // Change this to your desired color
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors
-                                    .grey, // Change this to your desired color
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),*/
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    TextButton(
-                      onPressed: () => controller.forgotPassword(),
-                      child: Text(
-                        'Password dimenticata?',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                        ),
+                        ],
                       ),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      spacing: 16,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            controller.login();
-                          },
-                          child: Text('Login'),
-                          style: ButtonStyle(
-                            shape: WidgetStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
+                    SizedBox(
+                      width: 300,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        spacing: 16,
+                        children: [
+                          FilledButton(
+                            onPressed: () {
+                              controller.login();
+                            },
+                            style: ButtonStyle(
+                              minimumSize:
+                                  WidgetStateProperty.all(Size(300, 48)),
+                              shape: WidgetStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
                               ),
                             ),
+                            child: Text('Entra'),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            controller.register();
-                          },
-                          child: Text('Register'),
-                        ),
-                      ],
-                    ),
+                          FilledButton(
+                            onPressed: () {
+                              controller.register();
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  WidgetStateProperty.all(Colors.black),
+                              minimumSize:
+                                  WidgetStateProperty.all(Size(300, 48)),
+                              shape: WidgetStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                            child: Text('Registrati'),
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
