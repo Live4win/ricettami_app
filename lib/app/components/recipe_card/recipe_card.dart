dc8720374b4dd2d070c01791edc6e9f27fe825e9
Emmanuel Baah <baahemmanuel@hotmail.it>
2025-02-15 04:51:34 +0100

    better responsiveness

--- lib/app/components/recipe_card/recipe_card.dart
+++ lib/app/components/recipe_card/recipe_card.dart
@@ -9,110 +9,9 @@ class RecipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*return InkWell(
-      onTap: () {},
-      child: Container(
-        //constraints: BoxConstraints(),
-        clipBehavior: Clip.hardEdge,
-        padding: EdgeInsets.symmetric(horizontal: 42, vertical: 24),
-        //height: 250,
-        //width: 8400,
-
-        constraints: BoxConstraints.tightFor(width: 650), // does not work
-        decoration: BoxDecoration(
-          //color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(
-            color: Colors.grey,
-            width: 1,
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 24,
-          children: [
-            Text(
-              'Pesto alla genovese',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      spacing: 24,
-                      children: [
-                        Icon(
-                          RicettamiIcons.chef_hat,
-                          size: 44,
-                        ),
-                        Column(
-                          spacing: 8,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 310,
-                              child: Text(
-                                'Ricetta 1: Notare come pu; essere fatta una cpsa bellissima del genere bla abla',
-                                //maxLines: 2,
-                                softWrap: true,
-                                overflow: TextOverflow.clip,
-                                textAlign: TextAlign.justify,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 310,
-                              child: Text(
-                                'Ricetta 1: Notare come pu; essere fatta una cpsa bellissima del genere bla abla',
-                                maxLines: 2,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.justify,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {}, icon: Icon(Icons.ac_unit)),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.ac_unit)),
-                      ],
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-    */
-
     return InkWell(
       onTap: () {},
       child: Container(
-        width: width,
-        height: height,
-        clipBehavior: Clip.hardEdge,
         padding: EdgeInsets.symmetric(horizontal: 42, vertical: 24),
         decoration: BoxDecoration(
           //color: Colors.white,
@@ -122,9 +21,11 @@ class RecipeCard extends StatelessWidget {
             width: 1,
           ),
         ),
+        //alignment: AlignmentDirectional.topStart,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisSize: MainAxisSize.max,
           spacing: 24,
           children: [
             Text(
@@ -135,76 +36,89 @@ class RecipeCard extends StatelessWidget {
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      width: (width ?? context.width) /
-                          2.5, // Adjust the width as needed
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            RicettamiIcons.chef_hat,
-                            size: 44,
-                          ),
-                          SizedBox(
-                              width: 24), // Add spacing between icon and text
-                          Flexible(
-                            child: Column(
-                              spacing: 8,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Recipe descriptionRecipe descriptionRecipe descriptionRecipe descriptionRecipe descriptionRecipe descriptionRecipe description',
-                                  overflow: TextOverflow.ellipsis,
-                                  softWrap: true,
-                                  maxLines: 2,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  'Recipe content',
-                                  overflow: TextOverflow.ellipsis,
-                                  softWrap: true,
-                                  maxLines: 2,
-                                ),
-                              ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Flexible(
+                    flex: 2,
+                    //fit: FlexFit.tight,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          spacing: 24,
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              //fit: FlexFit.tight,
+                              child: Icon(
+                                RicettamiIcons.chef_hat,
+                                size: 44,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
+                            Flexible(
+                              flex: 3,
+                              fit: FlexFit.tight,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                spacing: 8,
+                                children: [
+                                  Text(
+                                    'Ricetta 1: Notare come pu; essere fatta una cpsa bellissima del genere bla abla',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    'ciaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociao',
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-
-                // Trailing icons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Trailing icons go here
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        RicettamiIcons.edit,
-                        size: 32,
-                      ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            // Trailing icons go here
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                RicettamiIcons.edit,
+                                size: 32,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                RicettamiIcons.delete,
+                                size: 32,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        RicettamiIcons.delete,
-                        size: 32,
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            )
+                  )
+                ])
           ],
         ),
       ),
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -79,25 +79,28 @@ class HomeView extends GetView<HomeController> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         //crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Benvenuto Emmanuel, ecco le tue ricette',
-                                style: TextStyle(
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.bold,
+                          Flexible(
+                            fit: FlexFit.tight,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Benvenuto Emmanuel, ecco le tue ricette',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                'Crea, modifica e condividi le tue ricette',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal,
+                                Text(
+                                  'Crea, modifica e condividi le tue ricette',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                           Column(
                             children: [
@@ -110,195 +113,32 @@ class HomeView extends GetView<HomeController> {
                         ],
                       ),
                     ),
-                    SizedBox(
-                        //color: Colors.blue,
-                        height: 450,
-                        /*width: 800,*/ child: ResponsiveGridList(
-                          desiredItemWidth: 500,
-                          shrinkWrap: true,
-                          minSpacing: 44.0,
-                          //shrinkWrap: true,
-                          /*crossAxisCount: (context.width >= 1775) ? 3 : 2,
-                          childAspectRatio: 3,
-                          mainAxisSpacing: 44,
-                          crossAxisSpacing: 44,*/
+                    Expanded(
+                      child: ResponsiveGridList(
+                        desiredItemWidth: 400,
+                        minSpacing: 44.0,
+                        //shrinkWrap: true,
+                        //minSpacing: 44.0,
+                        //shrinkWrap: true,
+                        /*crossAxisCount: (context.width >= 1775) ? 3 : 2,
+                        childAspectRatio: 3,
+                        mainAxisSpacing: 44,
+                        crossAxisSpacing: 44,*/
+                        //crossAxisCount: 2,
+                        //mainAxisSpacing: 44,
+                        //crossAxisSpacing: 44,
+                        //childAspectRatio: context.width / 550,
 
-                          ///padding: EdgeInsets.all(44),
-                          children: [
-                            RecipeCard(),
-                            InkWell(
-                              onTap: () {},
-                              child: Container(
-                                //constraints: BoxConstraints(),
-                                clipBehavior: Clip.hardEdge,
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 42, vertical: 24),
-                                //height: 250,
-                                //width: 400,
-
-                                constraints:
-                                    BoxConstraints.tightFor(width: 250),
-                                decoration: BoxDecoration(
-                                  //color: Colors.white,
-                                  borderRadius: BorderRadius.circular(8),
-                                  border: Border.all(
-                                    color: Colors.grey,
-                                    width: 1,
-                                  ),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  spacing: 24,
-                                  children: [
-                                    Text(
-                                      'Pesto alla genovese',
-                                      style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Column(
-                                          children: [
-                                            Row(
-                                              spacing: 24,
-                                              children: [
-                                                Icon(
-                                                  RicettamiIcons.chef_hat,
-                                                  size: 44,
-                                                ),
-                                                Column(
-                                                  spacing: 8,
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  children: [
-                                                    SizedBox(
-                                                      width: 310,
-                                                      child: Text(
-                                                        'Ricetta 1: Notare come pu; essere fatta una cpsa bellissima del genere bla abla',
-                                                        //maxLines: 2,
-                                                        softWrap: true,
-                                                        overflow:
-                                                            TextOverflow.clip,
-                                                        textAlign:
-                                                            TextAlign.justify,
-                                                        style: TextStyle(
-                                                          fontSize: 16,
-                                                          fontWeight:
-                                                              FontWeight.bold,
-                                                        ),
-                                                      ),
-                                                    ),
-                                                    SizedBox(
-                                                      width: 310,
-                                                      child: Text(
-                                                        'Ricetta 1: Notare come pu; essere fatta una cpsa bellissima del genere bla abla',
-                                                        maxLines: 2,
-                                                        softWrap: true,
-                                                        overflow: TextOverflow
-                                                            .ellipsis,
-                                                        textAlign:
-                                                            TextAlign.justify,
-                                                        style: TextStyle(
-                                                          fontSize: 16,
-                                                          fontWeight:
-                                                              FontWeight.normal,
-                                                        ),
-                                                      ),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                        Column(
-                                          children: [
-                                            Row(
-                                              children: [
-                                                IconButton(
-                                                    onPressed: () {},
-                                                    icon: Icon(Icons.ac_unit)),
-                                                IconButton(
-                                                    onPressed: () {},
-                                                    icon: Icon(Icons.ac_unit)),
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                        /*Row(
-                                          spacing: 24,
-                                          children: [
-                                            Icon(
-                                              RicettamiIcons.chef_hat,
-                                              size: 44,
-                                            ),
-                                            Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              spacing: 8,
-                                              children: [
-                                                SizedBox(
-                                                  //width: 320,
-                                                  child: Text(
-                                                    'Ricetta 1: Notare come pu; essere fatta una cpsa bellissima del genere bla abla',
-                                                    //maxLines: 2,
-                                                    softWrap: true,
-                                                    overflow:
-                                                        TextOverflow.ellipsis,
-                                                    textAlign:
-                                                        TextAlign.justify,
-                                                    style: TextStyle(
-                                                      fontSize: 16,
-                                                      fontWeight:
-                                                          FontWeight.bold,
-                                                    ),
-                                                  ),
-                                                ),
-                                                SizedBox(
-                                                  //width: 320,
-                                                  child: Text(
-                                                    'Descrizione ricetta 1, guarda come psuef sdb fpsbfp b dsbfiup bdfsifbsdifbs b pfdsibuf spdb fsdbp',
-                                                    //maxLines: 2,
-                                                    softWrap: true,
-                                                    overflow:
-                                                        TextOverflow.ellipsis,
-                                                    textAlign:
-                                                        TextAlign.justify,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),*/
-                                        /*Row(
-                                          children: [
-                                            IconButton(
-                                                onPressed: () {},
-                                                icon: Icon(
-                                                  Icons.edit_outlined,
-                                                  size: 44,
-                                                )),
-                                            IconButton(
-                                                onPressed: () {},
-                                                icon: Icon(
-                                                  RicettamiIcons.delete,
-                                                  size: 44,
-                                                )),
-                                          ],
-                                        )*/
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        )),
+                        ///padding: EdgeInsets.all(44),
+                        children: [
+                          RecipeCard(),
+                          RecipeCard(),
+                          RecipeCard(),
+                          RecipeCard(),
+                          RecipeCard(),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
